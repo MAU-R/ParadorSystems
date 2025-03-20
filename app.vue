469a08b8e0e185c54f24f6ce74c95ef155817c6b
MAU-R <mrdevs@outlook.es>
2025-03-20 13:10:06 -0600

    Add tailwind and PRIMEVue

--- app.vue
+++ app.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+    <div class="bg-white w-full h-[100vh]"></div>
+<Button label="Submit" />
 </template>
+
+<script lang="ts" setup>
+import Button from 'primevue/button';
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,28 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+import tailwindcss from "@tailwindcss/vite";
+import Aura from '@primeuix/themes/aura';
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-
+  css: ['~/assets/css/main.css'],
   modules: [
     '@nuxt/content',
     '@nuxt/eslint',
     '@nuxt/fonts',
-    '@nuxt/icon'
-  ]
+    '@nuxt/icon',
+    '@primevue/nuxt-module'
+  ],
+    primevue: {
+      options: {
+        theme: {
+            preset: Aura
+        }
+    }
+    },
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ]
+  },
 })
