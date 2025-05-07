5e3fd6ce28711236f9498e28cee2c51d07451301
MAU-R <mrdevs@outlook.es>
2025-05-07 17:58:56 -0600

    ADDED the suit component for the room section

--- components/SuitRoom.vue
+++ components/SuitRoom.vue
@@ -10,8 +10,14 @@
             <h4>{{ suit.title }}</h4>
             <p>{{ suit.description }}</p>
         </div >
-        <div class="w-23/10">
+        <div class="w-23/10 flex gap-12 w-8/10 mt-auto mb-auto columns-3">
+            <div
+            v-for="service in room.services" :key="service.id"
+            class="rounded-3xl bg-(--blue-light)/50 flex gap-2 justify-around p-2 sm:p-1 align-center">
             
+            <Icon :name="service.icon" style="color: var(--blue-light); height: 100%; font-weight: 900;" />
+            <p class="xl:text-md md:text-sm sm:text-xs">{{ service.quantity ?? service.label ?? ""}}</p>
+            </div>
         </div>
         </div>
     </article>
